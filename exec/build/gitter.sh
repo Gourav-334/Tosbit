4e598189599776bec76d1987eb36d397d9e4f68b
Gourav Kumar Mallick <12b.39gourav@gmail.com>
2025-03-08 10:21:25 +0000

    Proper commenting done to socket programs -> client.c, server.c

--- exec/build/gitter.sh
+++ exec/build/gitter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add --all
-git commit -m "This bug makes me cry every time I see it, it mocks me."
+git commit -m "Proper commenting done to socket programs -> client.c, server.c"
 git pull origin gouravTest
 git push origin gouravTest
 git checkout main
